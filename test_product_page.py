9b96923460d107a4b4f9499afb2ab7addf949eec
Yulia <32438782+Coriolan8@users.noreply.github.com>
2021-02-12 23:14:05 +0300

    Add parament for test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,11 @@
-import time
-
+import pytest
 from pages.product_page import ProductPage
 
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', ['0','1','2','3','4','5','6',
+                                  pytest.param("7", marks=pytest.mark.xfail),
+                                  '8', '9'])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     product_page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     product_page.open()
     product_page.add_item_in_basket()
