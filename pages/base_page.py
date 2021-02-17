8eba57712efdb24dc24af88b6dc1e374db3b9d8f
Yulia <32438782+Coriolan8@users.noreply.github.com>
2021-02-17 21:55:12 +0300

    Add two tests for product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,8 +2,9 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support import expected_conditions as EC
 import math
-
 from selenium.webdriver.support.wait import WebDriverWait
+
+from pages.locators import BasePageLocators
 
 
 class BasePage():
@@ -52,4 +53,14 @@ class BasePage():
             return False
 
         return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+        # alert = self.browser.switch_to.alert
+        # alert.accept()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
